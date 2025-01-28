893230e8b67f17edd648544ae7c9b5cdc0a7dfce
onettboots <blackcocopet@gmail.com>
2025-01-29 00:29:23 +0700

    fs: fixups susfs patch
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,11 +3,9 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
 extern int susfs_spoof_proc_cmdline(struct seq_file *m);
 #endif
-
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
@@ -19,3 +17,22 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
+
+static int cmdline_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, cmdline_proc_show, NULL);
+}
+
+static const struct file_operations cmdline_proc_fops = {
+	.open		= cmdline_proc_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int __init proc_cmdline_init(void)
+{
+	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+	return 0;
+}
+fs_initcall(proc_cmdline_init);
